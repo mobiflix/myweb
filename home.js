a57628077658db19e3c7d3a7cc22b9906033797d
mobiflix <mobiflixhub@gmail.com>
2026-09-13 05:42:03 +0800

    Update home.js

--- home.js
+++ home.js
@@ -12,8 +12,7 @@ const MOVIE_ENDPOINTS = [
 ];
 
 const SERIES_ENDPOINTS = [
-  { name: 'VidSrc.me', url: 'https://vidsrc.me/embed/tv/' },
-  { name: '2Embed', url: 'https://www.2embed.cc/embed/tv' }
+  { name: 'VidSrc.me', url: 'https://vidsrc.me/embed/tv/' }
 ];
 
 // ===== GENRE MAP =====
