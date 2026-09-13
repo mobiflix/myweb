c7e6d8a27f0082cd7c859264ed2f899f450aaf27
mobiflix <mobiflixhub@gmail.com>
2026-09-13 10:35:05 +0800

    Update home.js

--- home.js
+++ home.js
@@ -5,8 +5,8 @@ const IMG_W500 = 'https://image.tmdb.org/t/p/w500';
 
 // ===== ENDPOINTS =====
 const MOVIE_ENDPOINTS = [
-  { name: 'Zxcstream', url: 'https://zxcstream.xyz/player/movie/' },
   { name: 'VidLink', url: 'https://vidlink.pro/movie/' },
+  { name: 'Zxcstream', url: 'https://zxcstream.xyz/player/movie/' },
   { name: '111Movies', url: 'https://111movies.com/movie/' },
   { name: 'VidSrc.io', url: 'https://vidsrc.io/embed/movie/' },
   { name: '2Embed', url: 'https://www.2embed.cc/embed/' }
