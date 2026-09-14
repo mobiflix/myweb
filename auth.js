023e5cd1027ceba48e1025ea3df0e10c6d65c1ce
mobiflix <mobiflixhub@gmail.com>
2026-09-14 10:13:58 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -6,7 +6,7 @@
 
 // 👇 DITO MO ILALAGAY YUNG MGA USERNAME AT PASSWORD
 const USERS = [
-  { username: 'admin', password: 'mobiflix2026' }
+  { username: 'mobiflix', password: '8888' }
 ];
 
 // Ilang araw bago ma-expire yung login session
